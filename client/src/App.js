ee58fd06c1ba4ff51a44b974ead865d554ee9e16
Max David <89958956+maxdavidsouza@users.noreply.github.com>
2023-04-11 06:17:18 -0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//Importando os Componentes Utilizados
+import Pong from "./components/Pong";
+
+//Criando o Componente Core da Aplicação
+function App() {
+  return (
+    <div>
+        <Pong/>
+    </div>
+  );
+}
+
+export default App;
